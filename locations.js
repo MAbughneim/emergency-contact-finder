d8c3b8a2fb662e9fe791783373366f1062fce2a7
MAbughneim <Mabughneim@customelectricusa.com>
2026-08-28 08:01:11 -0400

    Update locations array with new test hospitals
    Removed several urgent care and emergency room entries from the locations array, while adding new test hospital entries.

--- locations.js
+++ locations.js
@@ -1,53 +1,41 @@
 const locations = [
+    {
+        name: "Hartford Test Hospital",
+        type: "Hospital",
+        town: "Hartford",
+        phone: "860-555-1111",
+        latitude: 41.7658,
+        longitude: -72.6734,
+        website: "https://www.google.com"
+    },
 
-{
-    type: "Urgent Care",
-    name: "Trinity Health Urgent Care Bloomfield",
-    town: "Bloomfield",
-    phone: "860-900-0941",
-    latitude: 41.816980,
-    longitude: -72.740010,
-    website: "https://www.trinityhealthofne.org/location/bloomfield-access-center"
-},
+    {
+        name: "Manchester Test Hospital",
+        type: "Hospital",
+        town: "Manchester",
+        phone: "860-555-2222",
+        latitude: 41.7759,
+        longitude: -72.5215,
+        website: "https://www.google.com"
+    },
 
-{
-    type: "Emergency Room",
-    name: "Manchester Memorial Hospital",
-    town: "Manchester",
-    phone: "860-646-1222",
-    latitude: 41.781250,
-    longitude: -72.526530,
-    website: "https://www.manchestermemorialhospital.org/locations/manchester-memorial-hospital/"
-},
+    {
+        name: "Bloomfield Test Hospital",
+        type: "Hospital",
+        town: "Bloomfield",
+        phone: "860-555-3333",
+        latitude: 41.8265,
+        longitude: -72.7301,
+        website: "https://www.google.com"
+    },
 
-{
-    type: "Emergency Room",
-    name: "Hartford Hospital",
-    town: "Hartford",
-    phone: "860-545-5000",
-    latitude: 41.754383,
-    longitude: -72.680161,
-    website: "https://hartfordhospital.org/"
-},
-
-{
-    type: "Urgent Care",
-    name: "AFC Urgent Care West Hartford",
-    town: "West Hartford",
-    phone: "860-986-6440",
-    latitude: 41.758700,
-    longitude: -72.740800,
-    website: "https://www.afcurgentcare.com/west-hartford/"
-},
-
-{
-    type: "Emergency Room",
-    name: "Hospital of Central Connecticut",
-    town: "New Britain",
-    phone: "860-224-5671",
-    latitude: 41.660991,
-    longitude: -72.787420,
-    website: "https://thocc.org/locations/new-britain-general-campus"
-}
-
+    {
+        name: "New Britain Test Hospital",
+        type: "Hospital",
+        town: "New Britain",
+        phone: "860-555-4444",
+        latitude: 41.6612,
+        longitude: -72.7795,
+        website: "https://www.google.com"
+    }
 ];
